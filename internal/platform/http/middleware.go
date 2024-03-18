0c6a63c41c8548edd1da3f4e613354a8027000f4
JUSSIAR <klokov.stas.022@mail.ru>
2024-03-18 09:30:27 +0300

    Fix

--- internal/platform/http/middleware.go
+++ internal/platform/http/middleware.go
@@ -4,10 +4,10 @@ import "net/http"
 
 func CorsMiddleware(next http.Handler) http.Handler {
 	fn := func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PATCH, DELETE, PUT")
 		w.Header().Set("Access-Control-Allow-Headers", "*")
-		w.Header().Set("Access-Control-Allow-Credentials", "true")
+		w.Header().Set("Access-Control-Allow-Credentials", "false")
 
 		if r.Method == "OPTIONS" {
 			w.WriteHeader(http.StatusOK)
